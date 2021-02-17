ad60ebe7d7e8e3457e750e4a9e636bdb13f4438f
Andrew5445 <47328547+Andrew5445@users.noreply.github.com>
2021-02-17 14:52:07 +0100

    Update Api.ts

--- src/components/use/Api.ts
+++ src/components/use/Api.ts
@@ -1,5 +1,5 @@
-const weatherApiKey = "fe993687a12ceb45e37616d5caf325f5";
-const geocodingApiKey = "AIzaSyCAs_LwIjruLg3X93JeXNzawOL5C2oxOwI";
+const weatherApiKey = "";
+const geocodingApiKey = "";
 
 export default {
     weatherApiKey: weatherApiKey,
